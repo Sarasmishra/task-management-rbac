3720b475a304b7492111a1e0c095981b9f92a9a2
Sarasmishra <Sarasmishra8100@gmail.com>
2026-06-10 22:38:12 +0530

    Implemented the cors in backend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const authRoutes = require("./src/routes/authRoutes");
 const taskRoutes = require("./src/routes/taskRoutes");
 const adminRoutes = require("./src/routes/adminRoutes");
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks",taskRoutes);
 app.use("/api/admin",adminRoutes);
